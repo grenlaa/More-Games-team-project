31b2b920ad32314338174764158d216dce5fd9bc
Фурсов Георгий Алексеевич <51056540+grenlaa@users.noreply.github.com>
2025-04-24 16:58:21 +0300

    add version multiplayer

---
+++ MoreGames_multiplayer/MoreGames/Form1.cs
@@ -0,0 +1,52 @@
+namespace MoreGames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RememberGame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FlappyBird_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TicTacToe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Memo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tetris_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connect_room_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Create_room_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            _webHost?.StopAsync().GetAwaiter().GetResult(); // Остановка при закрытии
+            base.OnFormClosing(e);
+        }
+    }
+}
