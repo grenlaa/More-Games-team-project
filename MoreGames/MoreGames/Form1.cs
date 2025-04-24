ffd3f78084f0351f5e997eb0bb65e0b8c95f1765
Фурсов Георгий Алексеевич <51056540+grenlaa@users.noreply.github.com>
2025-04-24 13:01:42 +0300

    rename project

---
+++ MoreGames/MoreGames/Form1.cs
@@ -0,0 +1,45 @@
+namespace MoreGames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RememberGame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FlappyBird_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TicTacToe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Memo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tetris_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connect_room_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Create_room_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
