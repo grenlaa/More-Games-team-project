e3640c6ab059c6dbbd6a90ee57b67b7c4ff640db
SashaMixaylov <alexandr_mixalov@mail.ru>
2025-04-28 19:43:16 +0300

    Добавлена игра Подберите пары

---
+++ MoreGames/MoreGames/Form4_test.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WinFormsApp1_18._04._2025
+{
+    partial class Form1
+    {/// <summary>
+     /// Required designer variable.
+     /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            menuStrip1 = new MenuStrip();
+            GamesToolStripMenuItem = new ToolStripMenuItem();
+            NewGameToolStripMenuItem = new ToolStripMenuItem();
+            ExitToolStripMenuItem = new ToolStripMenuItem();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(99, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 123);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(229, 17);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 123);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(355, 17);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(100, 123);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(489, 17);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(100, 123);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(99, 159);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(100, 123);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(229, 159);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(100, 123);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(355, 159);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(100, 123);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 6;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(489, 159);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(100, 123);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 7;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Location = new Point(99, 304);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(100, 123);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 8;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Location = new Point(229, 304);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(100, 123);
+            pictureBox10.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox10.TabIndex = 9;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Location = new Point(355, 304);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(100, 123);
+            pictureBox11.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox11.TabIndex = 10;
+            pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Location = new Point(489, 304);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(100, 123);
+            pictureBox12.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox12.TabIndex = 11;
+            pictureBox12.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Dock = DockStyle.Right;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { GamesToolStripMenuItem, NewGameToolStripMenuItem, ExitToolStripMenuItem });
+            menuStrip1.Location = new Point(713, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.RenderMode = ToolStripRenderMode.System;
+            menuStrip1.Size = new Size(87, 450);
+            menuStrip1.TabIndex = 12;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // GamesToolStripMenuItem
+            // 
+            GamesToolStripMenuItem.Name = "GamesToolStripMenuItem";
+            GamesToolStripMenuItem.Size = new Size(74, 19);
+            GamesToolStripMenuItem.Text = "Играть";
+            GamesToolStripMenuItem.Click += GamesToolStripMenuItem_Click;
+            // 
+            // NewGameToolStripMenuItem
+            // 
+            NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem";
+            NewGameToolStripMenuItem.Size = new Size(74, 19);
+            NewGameToolStripMenuItem.Text = "Новая игра";
+            NewGameToolStripMenuItem.Click += NewGameToolStripMenuItem_Click;
+            // 
+            // ExitToolStripMenuItem
+            // 
+            ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            ExitToolStripMenuItem.Size = new Size(74, 19);
+            ExitToolStripMenuItem.Text = "Выход";
+            ExitToolStripMenuItem.Click += ExitToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem GamesToolStripMenuItem;
+        private ToolStripMenuItem NewGameToolStripMenuItem;
+        private ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ MoreGames/MoreGames/Form4_test.cs
@@ -0,0 +1,169 @@
+using System.Resources;
+using Timer = System.Threading.Timer;
+
+namespace WinFormsApp1_18._04._2025
+{
+    public partial class Form1 : Form
+    {
+
+        public PictureBox[] pictureBoxes;
+        public int[] cardValues;
+        public int firstChoice = -1;
+        private int secondChoice = -1;
+        private bool isProcessing = false;
+        private ResourceManager resourceManager;
+        public Form1()
+        {
+            InitializeComponent();
+
+            resourceManager = new ResourceManager(typeof(Resource1));
+            pictureBoxes = new PictureBox[12];
+            cardValues = new int[12];
+
+            int[] values = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+            Random rand = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                int index = rand.Next(values.Length);
+                cardValues[i] = values[index];
+                values = RemoveAt(values, index);
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                pictureBoxes[i] = new PictureBox
+                {
+                    Width = 80,
+                    Height = 100,
+                    Left = (i % 4) * 90 + 150,
+                    Top = (i / 4) * 110 + 50,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    BackColor = System.Drawing.Color.LightGray
+                };
+                pictureBoxes[i].Click += pictureBox1_Click;
+                Controls.Add(pictureBoxes[i]);
+            }
+
+            MenuStrip menuStrip = new MenuStrip();
+            ToolStripMenuItem gameMenu = new ToolStripMenuItem("Игра");
+
+            ToolStripMenuItem newGameItem = new ToolStripMenuItem("Новая игра");
+            newGameItem.Click += NewGameToolStripMenuItem_Click;
+
+            ToolStripMenuItem exitItem = new ToolStripMenuItem("Выход");
+            exitItem.Click += ExitToolStripMenuItem_Click;
+
+            gameMenu.DropDownItems.AddRange(new ToolStripMenuItem[] { newGameItem, exitItem });
+            menuStrip.Items.Add(gameMenu);
+            Controls.Add(menuStrip);
+        }
+
+
+        private void GamesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (isProcessing) return;
+
+            PictureBox clickedBox = (PictureBox)sender;
+            int index = Array.IndexOf(pictureBoxes, clickedBox);
+
+            if (clickedBox.Image == null)
+            {
+                byte[] imageData = (byte[])resourceManager.GetObject($"card{cardValues[index]}");
+
+                using (MemoryStream ms = new MemoryStream(imageData))
+                {
+                    clickedBox.Image = new Bitmap(ms);
+                }
+
+                if (firstChoice == -1)
+                {
+                    firstChoice = index;
+                }
+                else
+                {
+                    secondChoice = index;
+                    isProcessing = true;
+
+                    if (cardValues[firstChoice] == cardValues[secondChoice])
+                    {
+                        // Карточки совпали - оставляем их открытыми
+                        firstChoice = -1;
+                        secondChoice = -1;
+                        isProcessing = false;
+
+                        // Проверяем, все ли пары найдены
+                        bool allMatched = true;
+                        foreach (PictureBox pb in pictureBoxes)
+                        {
+                            if (pb.Image == null)
+                            {
+                                allMatched = false;
+                                break;
+                            }
+                        }
+
+                        if (allMatched)
+                        {
+                            MessageBox.Show("Поздравляем! Вы нашли все пары!");
+                        }
+                    }
+                    else
+                    {
+                        // Карточки не совпали - закрываем их через секунду
+
+                        timer1.Interval = 1000;
+                        timer1.Tick += (tSender, args) =>
+                        {
+                            if (firstChoice >= 0 && firstChoice < pictureBoxes.Length)
+                            {
+                                clickedBox.Image = null;
+                                pictureBoxes[firstChoice].Image = null;
+                            }
+                            timer1.Stop();
+                            firstChoice = -1;
+                            secondChoice = -1;
+                            isProcessing = false;
+                        };
+                        timer1.Start();
+                    }
+                }
+            }
+        }
+
+        private void NewGameToolStripMenuItem_Click(object sender, EventArgs e)   //Новая игра
+        {
+            foreach (PictureBox pb in pictureBoxes)
+            {
+                pb.Image = null;
+            }
+
+            int[] values = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+            Random rand = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                int index = rand.Next(values.Length);
+                cardValues[i] = values[index];
+                values = RemoveAt(values, index);
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)  //Выход
+        {
+            Application.Exit();
+        }
+
+        private static T[] RemoveAt<T>(T[] source, int index)
+        {
+            T[] dest = new T[source.Length - 1];
+            Array.Copy(source, 0, dest, 0, index);
+            Array.Copy(source, index + 1, dest, index, source.Length - index - 1);
+            return dest;
+        }
+    }
+
+}
